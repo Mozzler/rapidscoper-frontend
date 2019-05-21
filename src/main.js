421c43c08cc8cb395c84a585e61c41bb2bcf2b3d
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-05-21 13:51:13 +0300

    fix linter errors

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ import router from './routes'
 import './plugins/vuetify'
 import './plugins/axios'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 let main = new Vue({
   el: '#app',
   store,
   router,
   render: h => h(App)
-})
+});
 
-export default main
+export default main;
