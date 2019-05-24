24ef1b1b01a78d26c14916ee9dc4e1ec684acaf5
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-05-24 17:22:56 +0300

    add offsets between cards

--- src/components/pages/dashboard/tabs/Billing.vue
+++ src/components/pages/dashboard/tabs/Billing.vue
@@ -1,24 +1,25 @@
 <template>
-  <v-layout row wrap align-center>
-    <v-flex xs7>
-      <v-card class="billing-card">
-        <div class="billing-card__title">Your Plan</div>
-        <div>You are on a Basic Plan.</div>
-        <div class="text-greyed billing-card__text">It's free forever.</div>
-        <div class="billing-card__action">Change Plan</div>
-      </v-card>
-      <v-card class="billing-card">
-        <div class="billing-card__title">Payment Details</div>
-        <div>You are on a Basic Plan.</div>
-        <div class="mt-3">
-          <master-card-icon />
-          <span class="ml-2">**** **** **** 4149</span>
-        </div>
-        <div class="billing-card__action">Update payment info</div>
-      </v-card>
+  <v-layout row wrap>
+    <v-flex xs12 sm12 md7>
+      <v-layout column wrap>
+        <v-card class="billing-card">
+          <div class="billing-card__title">Your Plan</div>
+          <div>You are on a Basic Plan.</div>
+          <div class="text-greyed billing-card__text">It's free forever.</div>
+          <div class="billing-card__action">Change Plan</div>
+        </v-card>
+        <v-card class="billing-card">
+          <div class="billing-card__title">Payment Details</div>
+          <div>
+            <master-card-icon class="billing-card__logo"/>
+            <span class="ml-2">**** **** **** 4149</span>
+          </div>
+          <div class="billing-card__action">Update payment info</div>
+        </v-card>
+      </v-layout>
     </v-flex>
-    <v-flex xs5>
-      <v-card class="billing-card">
+    <v-flex sm12 md5>
+      <v-card class="billing-card offset">
         <div class="billing-card__title">Upgrade to Premium</div>
         <div>Unlock all features of RapidScoper include unlimited projects and export by upgrading to Premium Subscription Plan</div>
         <v-btn large class="btn-rapid primary mt-4">
