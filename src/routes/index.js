fc7a053b676a663868ab72a331561e41ef8851f6
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-06-25 10:54:58 +0300

    comment route validation to 'Create an Account' page

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,11 +25,11 @@ router.beforeEach((to, from, next) => {
   const user = store.state.auth.user;
   const authenticated = user !== null;
 
-  if (authenticated && user.firstName && to.name === 'create-account') {
+  /*if (authenticated && user.firstName && to.name === 'create-account') {
     next('/');
   } else if (authenticated && !user.firstName && to.name !== 'create-account') {
     next('/create-account');
-  } else if (to.meta.guest && authenticated) {
+  } else */if (to.meta.guest && authenticated) {
     next('/');
   } else if (to.meta.requiresAuth && !authenticated) {
     next('/signup');
