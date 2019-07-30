34a161d1a52f4f645420904c8bafcc3485981913
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-07-30 13:42:32 +0300

    fix sidebar icon styles

--- src/components/particles/lists/IconList.vue
+++ src/components/particles/lists/IconList.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="sidebar__title sidebar__title--centered">{{ title }}</div>
     <v-list>
-      <v-list-tile v-for="(item, key) in list" :key="key">
+      <v-list-tile v-for="(item, key) in list" :key="key"
+                   @click="() => $emit('click', key)">
         <v-list-tile-content>
           <v-list-tile-title class="sidebar__icon"
-                             :class="{'sidebar__icon--active': activeIcon === key}"
-                             @click="() => $emit('click', key)">
+                             :class="{'sidebar__icon--active': activeIcon === key}">
             <v-icon v-if="item">{{ key }}</v-icon>
             <component :is="key" v-else />
           </v-list-tile-title>
