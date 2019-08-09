6200059539032bf64e695fa683e2ee0b4ced75d9
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-08-09 08:56:38 +0300

    added scroll

--- src/components/particles/layouts/StoryContentStatic.vue
+++ src/components/particles/layouts/StoryContentStatic.vue
@@ -1,34 +1,48 @@
 <template>
-<div class="content-container">
-  <v-layout align-start justify-center row fill-height>
-    <div class="content" ref="scrollable-layout">
-      <stories />
-
-      <h1 class="content-container__header mt-3">summary</h1>
-      <div class="user-story__block">
-        <h1> Estimate for priority </h1>
-        <div class="mt-3 user-story__wysiwyg">
-          Time estimate for each priority.
+  <div class="scrollable-layout">
+    <div class="content-container">
+      <v-layout align-start justify-center row fill-height>
+        <div class="content">
+          <stories />
         </div>
-        <summary-list
-          total="true"
-          collection="priority" />
-      </div>
-      <div class="user-story__block">
-        <h1> Estimate for labels </h1>
-        <div class="mt-3 user-story__wysiwyg">
-          Time estimate for each label.
-        </div>
-        <summary-list
-          label-cls="tool-block__label rounded"
-          collection="labels" />
-      </div>
-
-      <h1 class="content-container__header">dictionary</h1>
-      <dictionary-static />
+      </v-layout>
     </div>
-  </v-layout>
-</div>
+
+    <div class="content-container">
+      <v-layout align-start justify-center row fill-height>
+        <div class="content">
+          <h1 class="content-container__header mt-3">summary</h1>
+          <div class="user-story__block">
+            <h1> Estimate for priority </h1>
+            <div class="mt-3 user-story__wysiwyg">
+              Time estimate for each priority.
+            </div>
+            <summary-list
+              total="true"
+              collection="priority" />
+          </div>
+          <div class="user-story__block">
+            <h1> Estimate for labels </h1>
+            <div class="mt-3 user-story__wysiwyg">
+              Time estimate for each label.
+            </div>
+            <summary-list
+              label-cls="tool-block__label rounded"
+              collection="labels" />
+          </div>
+        </div>
+      </v-layout>
+    </div>
+
+    <div class="content-container">
+      <v-layout align-start justify-center row fill-height>
+        <div class="content">
+          <h1 class="content-container__header">dictionary</h1>
+          <dictionary-static />
+        </div>
+      </v-layout>
+    </div>
+  </div>
 </template>
 
 <script>
