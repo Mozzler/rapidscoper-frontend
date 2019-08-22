88f39fd2de55d86d971e158f40067220ca4ba0ac
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-08-22 11:28:35 +0300

    added priority dropdown

--- src/components/particles/layouts/StoryContent.vue
+++ src/components/particles/layouts/StoryContent.vue
@@ -1,25 +1,47 @@
 <template>
   <div class="scrollable-layout" ref="scrollable-layout">
     <div class="content-filter" v-if="sidebarFilter">
-      <v-layout align-start justify-space-between row fill-height>
-        <v-flex>
-          <v-layout align-start justify-center row fill-height>
-            <v-flex class="filter__item--mr">Filters:</v-flex>
-            <v-flex class="filter__item filter__item--mr">
-              <div>Priorities: </div>
-              <v-icon>add</v-icon>
+      <v-layout align-center justify-space-between row fill-height>
+        <v-flex shrink>
+          <v-layout align-center justify-start row fill-height>
+            <v-flex class="filter__item--mr">
+              Filters:
             </v-flex>
             <v-flex class="filter__item filter__item--mr">
-              <div>Labels: </div>
+              <div class="mr-1">Priorities: </div>
+              <v-menu v-model="menu.priority"
+                      :nudge-bottom="30"
+                      :nudge-left="86">
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">add</v-icon>
+                </template>
+
+                <div class="dropdown-list">
+                  <v-list>
+                    <v-list-tile v-for="(item, key) in priorities" :key="key" @click="$emit('update', item)"
+                                 :class="{'v-list__tile--active': isEqual(selected, item)}">
+                      <v-list-tile-content>
+                        <v-list-tile-title :class="{'item-active': isEqual(selected, item)}">
+                          <span>{{ item }}</span>
+                        </v-list-tile-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </v-list>
+                </div>
+              </v-menu>
+            </v-flex>
+            <v-flex class="filter__item filter__item--mr">
+              <div class="mr-1">Labels: </div>
               <v-icon>add</v-icon>
             </v-flex>
-            <v-flex class="text-greyed">Estimate time: 12 hours</v-flex>
+            <!--<v-flex class="text-greyed">Estimate time: 12 hours</v-flex>-->
           </v-layout>
         </v-flex>
-        <v-flex>
-          <div>
+        <v-flex shrink>
+          <v-layout align-center justify-center class="filter__search">
+            <v-icon class="mr-2">search</v-icon>
             <input class="search-input" placeholder="Search" />
-          </div>
+          </v-layout>
         </v-flex>
       </v-layout>
     </div>
@@ -59,7 +81,11 @@ export default {
     return {
       message: null,
       scrollActive: false,
-      scrollSelector: '.user-story__block'
+      scrollSelector: '.user-story__block',
+      menu: {
+        priority: false,
+        labels: false
+      }
     };
   },
   beforeMount () {
@@ -92,6 +118,9 @@ export default {
     },
     sidebarFilter () {
       return this.$store.state.system.sidebarFilter;
+    },
+    priorities () {
+      return this.$store.state.story.priority;
     }
   },
   methods: {
@@ -112,6 +141,13 @@ export default {
           type: this.storyType
         }
       };
+    },
+    isEqual (left, right, field = 'id') {
+      if (typeof left === 'object' && typeof right === 'object') {
+        return left[field] === right[field];
+      }
+
+      return left === right;
     },
     async createSection () {
       const section = this.getSectionData();
