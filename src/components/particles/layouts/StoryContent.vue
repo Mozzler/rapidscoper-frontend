6bfc836b283f10b7f6dc441d9c1e38263eba872e
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-08-28 15:59:00 +0300

    fixed filter bar position

--- src/components/particles/layouts/StoryContent.vue
+++ src/components/particles/layouts/StoryContent.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="scrollable-layout" ref="scrollable-layout">
+  <div>
     <div class="content-filter" v-if="sidebarFilter">
       <sidebar-filters />
     </div>
-    <div class="content-container"
-         v-for="(story, index) in sections"
-         :key="index">
-      <v-layout align-start justify-center row fill-height>
-        <div class="content" ref="layout-content">
-          <story-item
-            :model="story"
-            ref="storyItem"
-            @show-error="value => message = value"/>
-        </div>
-        <hint />
-        <alert :message="message" />
-      </v-layout>
+    <div class="scrollable-layout" ref="scrollable-layout">
+      <div class="content-container"
+           v-for="(story, index) in sections"
+           :key="index">
+        <v-layout align-start justify-center row fill-height>
+          <div class="content" ref="layout-content">
+            <story-item
+              :model="story"
+              ref="storyItem"
+              @show-error="value => message = value"/>
+          </div>
+          <hint />
+          <alert :message="message" />
+        </v-layout>
+      </div>
     </div>
   </div>
 </template>
