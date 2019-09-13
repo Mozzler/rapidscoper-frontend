321e3c618a6c978cf775fbcc34b3aa533dc3fbb8
Chris Were <chris@mozzler.com.au>
2019-09-13 15:46:00 +0930

    Update sockets URL

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ module.exports = {
   API_URL: 'http://rapidscoper-api-staging.pcpt3paekh.ap-southeast-2.elasticbeanstalk.com/v1/',
   CLIENT_ID: 'client-id',
   CLIENT_SECRET: 'client-secret',
-  SOCKETS_URL: 'http://staging.rapidscoper.com:3000'
+  SOCKETS_URL: 'http://rapidscoper-socket-server-staging.ap-southeast-2.elasticbeanstalk.com:3000'
 };
