0a593585e102575617995e1407632d555694abe0
antonvakulenko93 <anton.vakulenko93@gmail.com>
2019-10-01 10:59:29 +0300

    added priority cancelling

--- src/components/particles/lists/Hint.vue
+++ src/components/particles/lists/Hint.vue
@@ -82,13 +82,13 @@ export default {
     items () {
       const keyword = this.filter ? this.filter.toLowerCase() : '';
 
-      return this. list ? this.list.filter(item => {
+      return this.list ? _.filter(this.list, item => {
         const data = this.getStrFromObj(item);
         return data.toLowerCase().includes(keyword, 0);
       }) : [];
     },
     inList () {
-      const filter = this.list.filter(item => {
+      const filter = _.filter(this.list, item => {
         return this.getStrFromObj(item) === this.filter.trim();
       });
       return Boolean(filter.length);
